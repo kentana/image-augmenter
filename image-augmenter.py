09ddbbfa87b066b2c93039994e241cceabae04c4
kentana <kentana.jp@gmail.com>
2018-06-12 08:18:17 +0900

    chore(py): add comments.

--- image-augmenter.py
+++ image-augmenter.py
@@ -4,10 +4,15 @@ from PIL import Image
 import tensorflow as tf
 
 # exts.
+# image-augmenter handles only png and jpg.
 EXTS = ['png', 'jpg']
 
 
 def get_images():
+    """
+    gets images in src directory.
+    """
+
     src_path = os.getcwd() + '/src'
 
     # get all images.
@@ -19,10 +24,18 @@ def get_images():
 
 
 def image_path(filename):
+    """
+    returns image's path from filename.
+    """
+
     return 'src/' + filename
 
 
 def permitted_image(filename):
+    """
+    reports image is permitted.
+    """
+
     ext = filename.split('.')[-1]
     return ext in EXTS
 
